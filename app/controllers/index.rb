a95011f02110fe2f99d6b14a5bf6dc45697c42e9
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 17:26:38 -0500

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,5 +5,21 @@ get '/' do
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect} right here.</code>"
+  response = params[:user_input]
+
+  if params[:user_input] == "Sorry grandma, gotta go"
+    @grandma = "wait, come back!"
+    redirect to('/?grandma=wait, come back!')
+    erb :result
+  elsif params[:user_input] == response.upcase
+    @grandma = "NOT SINCE 1938!"
+    redirect to('/?grandma=NOT SINCE 1938!')
+    erb :index
+  else 
+    @grandma = "SPEAK UP, SONNY!"
+    redirect to('/?grandma=SPEAK UP, SONNY!')
+    erb :index
+  end
 end
+
